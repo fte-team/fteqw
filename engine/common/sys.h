f121e3edd795a87b7b7f8ff9169a39fa5f8ec55c
Spoike <acceptthis@users.sourceforge.net>
2004-08-30 07:04:48 +0000

    fixing a warning the proper way.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@75 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/common/sys.h
+++ engine/common/sys.h
@@ -68,6 +68,9 @@ void Sys_Sleep (void);
 // called to yield for a little bit so as
 // not to hog cpu when paused or debugging
 
+void Sys_ServerActivity(void);
+//make window flash on the taskbar - someone said something/connected
+
 void Sys_SendKeyEvents (void);
 // Perform Key_Event () callbacks until the input que is empty
 

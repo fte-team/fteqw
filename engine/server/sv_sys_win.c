7ed4086431c07271f08b9ff297f419d5c6538cc1
Spoike <acceptthis@users.sourceforge.net>
2004-09-04 17:59:22 +0000

    ifdef server only so that it can be compiled as just annother file
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@130 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/server/sv_sys_win.c
+++ engine/server/sv_sys_win.c
@@ -20,6 +20,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/types.h>
 #include <sys/timeb.h>
 #include "qwsvdef.h"
+
+#ifdef SERVERONLY
+
 #include <winsock.h>
 #include <conio.h>
 
@@ -824,4 +827,5 @@ void CreateSampleService(qboolean create)
         return;
     }
 }
+#endif
 #endif

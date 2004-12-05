a82e237add5c6e6f574b49da431ce4473e71508a
Spoike <acceptthis@users.sourceforge.net>
2004-12-05 16:32:44 +0000

    Hash tables now have a case insensative option. (it acts as lowercase - don't mix them)
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@575 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/qclib/hash.c
+++ engine/qclib/hash.c
@@ -13,6 +13,19 @@ int Hash_Key(char *name, int modulus)
 		
 	return (int)(key%modulus);
 }
+int Hash_KeyInsensative(char *name, int modulus)
+{	//fixme: optimize.
+	unsigned int key;
+	for (key=0;*name; name++)
+	{
+		if (*name >= 'A' && *name <= 'Z')
+			key += ((key<<3) + (key>>28) + (*name-'A'+'a'));
+		else
+			key += ((key<<3) + (key>>28) + *name);
+	}
+		
+	return (int)(key%modulus);
+}
 
 void *Hash_Get(hashtable_t *table, char *name)
 {
@@ -24,6 +37,22 @@ void *Hash_Get(hashtable_t *table, char *name)
 	while(buck)
 	{
 		if (!STRCMP(name, buck->keystring))
+			return buck->data;
+
+		buck = buck->next;
+	}
+	return NULL;
+}
+void *Hash_GetInsensative(hashtable_t *table, char *name)
+{
+	int bucknum = Hash_Key(name, table->numbuckets);
+	bucket_t *buck;
+
+	buck = table->bucket[bucknum];
+
+	while(buck)
+	{
+		if (!stricmp(name, buck->keystring))
 			return buck->data;
 
 		buck = buck->next;
@@ -96,6 +125,17 @@ void *Hash_Add(hashtable_t *table, char *name, void *data)
 void *Hash_Add2(hashtable_t *table, char *name, void *data, bucket_t *buck)
 {
 	int bucknum = Hash_Key(name, table->numbuckets);
+
+	buck->data = data;
+	buck->keystring = name;
+	buck->next = table->bucket[bucknum];
+	table->bucket[bucknum] = buck;
+
+	return buck;
+}
+void *Hash_AddInsensative(hashtable_t *table, char *name, void *data, bucket_t *buck)
+{
+	int bucknum = Hash_KeyInsensative(name, table->numbuckets);
 
 	buck->data = data;
 	buck->keystring = name;

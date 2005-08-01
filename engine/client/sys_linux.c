7bdacbbaa369c2b64e4107d820aecfe0bc9248e0
Mark Olsen <isthisinuse@users.sourceforge.net>
2005-08-01 15:08:05 +0000

    Cleanup, removal of dead code, cosmetics, etc.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@1185 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/client/sys_linux.c
+++ engine/client/sys_linux.c
@@ -52,9 +52,6 @@ int nostdout = 0;
 char *basedir = ".";
 char *cachedir = "/tmp";
 
-cvar_t  sys_linerefresh = {"sys_linerefresh","0"};// set for entity display
-
-
 qboolean Sys_InitTerminal (void)	//we either have one or we don't.
 {
 	return true;
@@ -74,53 +71,6 @@ void Sys_DebugNumber(int y, int val)
 {
 }
 
-/*
-void Sys_Printf (char *fmt, ...)
-{
-	va_list		argptr;
-	char		text[1024];
-	
-	va_start (argptr,fmt);
-	_vsnprintf (text,sizeof(text)-1, fmt,argptr);
-	va_end (argptr);
-	fprintf(stderr, "%s", text);
-	
-	Con_Print (text);
-}
-
-void Sys_Printf (char *fmt, ...)
-{
-
-    va_list     argptr;
-    char        text[1024], *t_p;
-    int         l, r;
-
-    if (nostdout)
-        return;
-
-    va_start (argptr,fmt);
-    _vsnprintf (text,sizeof(text)-1, fmt,argptr);
-    va_end (argptr);
-
-    l = strlen(text);
-    t_p = text;
-
-// make sure everything goes through, even though we are non-blocking
-    while (l)
-    {
-        r = write (1, text, l);
-        if (r != l)
-            sleep (0);
-        if (r > 0)
-        {
-            t_p += r;
-            l -= r;
-        }
-    }
-
-}
-*/
-
 void Sys_Printf (char *fmt, ...)
 {
 	va_list		argptr;
@@ -134,8 +84,8 @@ void Sys_Printf (char *fmt, ...)
 	if (strlen(text) > sizeof(text))
 		Sys_Error("memory overwrite in Sys_Printf");
 
-    if (nostdout)
-        return;
+	if (nostdout)
+		return;
 
 	for (p = (unsigned char *)text; *p; p++)
 		if ((*p > 128 || *p < 32) && *p != 10 && *p != 13 && *p != 9)
@@ -147,7 +97,7 @@ void Sys_Printf (char *fmt, ...)
 void Sys_Quit (void)
 {
 	Host_Shutdown();
-    fcntl (0, F_SETFL, fcntl (0, F_GETFL, 0) & ~FNDELAY);
+	fcntl (0, F_SETFL, fcntl (0, F_GETFL, 0) & ~FNDELAY);
 	exit(0);
 }
 
@@ -160,32 +110,30 @@ void Sys_Init(void)
 
 void Sys_Error (const char *error, ...)
 { 
-    va_list     argptr;
-    char        string[1024];
+	va_list argptr;
+	char string[1024];
 
 // change stdin to non blocking
-    fcntl (0, F_SETFL, fcntl (0, F_GETFL, 0) & ~FNDELAY);
+	fcntl (0, F_SETFL, fcntl (0, F_GETFL, 0) & ~FNDELAY);
     
-    va_start (argptr,error);
-    _vsnprintf (string,sizeof(string)-1, error,argptr);
-    va_end (argptr);
+	va_start (argptr,error);
+	_vsnprintf (string,sizeof(string)-1, error,argptr);
+	va_end (argptr);
 	fprintf(stderr, "Error: %s\n", string);
 
-	
-	
 	Host_Shutdown ();
 	exit (1);
-
 } 
 
 void Sys_Warn (char *warning, ...)
 { 
-    va_list     argptr;
-    char        string[1024];
+	va_list argptr;
+	char string[1024];
     
-    va_start (argptr,warning);
-    _vsnprintf (string,sizeof(string)-1, warning,argptr);
-    va_end (argptr);
+	va_start (argptr,warning);
+	_vsnprintf (string,sizeof(string)-1, warning,argptr);
+	va_end (argptr);
+
 	fprintf(stderr, "Warning: %s", string);
 } 
 
@@ -209,8 +157,9 @@ int	Sys_FileTime (char *path)
 
 void Sys_mkdir (char *path)
 {
-    mkdir (path, 0777);
+	mkdir (path, 0777);
 }
+
 qboolean Sys_remove (char *path)
 {
 	return system(va("rm \"%s\"", path));
@@ -218,9 +167,8 @@ qboolean Sys_remove (char *path)
 
 int Sys_FileOpenRead (char *path, int *handle)
 {
-	int	h;
-	struct stat	fileinfo;
-    
+	int h;
+	struct stat fileinfo;
 	
 	h = open (path, O_RDONLY, 0666);
 	*handle = h;
@@ -235,12 +183,11 @@ int Sys_FileOpenRead (char *path, int *handle)
 
 int Sys_FileOpenWrite (char *path)
 {
-	int     handle;
+	int handle;
 
 	umask (0);
 	
-	handle = open(path,O_RDWR | O_CREAT | O_TRUNC
-	, 0666);
+	handle = open(path,O_RDWR | O_CREAT | O_TRUNC, 0666);
 
 	if (handle == -1)
 		Sys_Error ("Error opening %s: %s", path,strerror(errno));
@@ -265,48 +212,26 @@ void Sys_FileSeek (int handle, int position)
 
 int Sys_FileRead (int handle, void *dest, int count)
 {
-    return read (handle, dest, count);
+	return read (handle, dest, count);
 }
 
 void Sys_DebugLog(char *file, char *fmt, ...)
 {
-    va_list argptr; 
-    static char data[1024];
-    int fd;
+	va_list argptr; 
+	static char data[1024];
+	int fd;
     
-    va_start(argptr, fmt);
-    _vsnprintf (data,sizeof(data)-1, fmt, argptr);
-    va_end(argptr);
+	va_start(argptr, fmt);
+	_vsnprintf (data,sizeof(data)-1, fmt, argptr);
+	va_end(argptr);
 
-    if (strlen(data) > sizeof(data))
-	    Sys_Error("Sys_DebugLog's buffer was stomped\n");
-//    fd = open(file, O_WRONLY | O_BINARY | O_CREAT | O_APPEND, 0666);
-    fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0666);
-    write(fd, data, strlen(data));
-    close(fd);
-}
+	if (strlen(data) > sizeof(data))
+		Sys_Error("Sys_DebugLog's buffer was stomped\n");
 
-void Sys_EditFile(char *filename)
-{
-
-	char cmd[256];
-	char *term;
-	char *editor;
-
-	term = getenv("TERM");
-	if (term && !strcmp(term, "xterm"))
-	{
-		editor = getenv("VISUAL");
-		if (!editor)
-			editor = getenv("EDITOR");
-		if (!editor)
-			editor = getenv("EDIT");
-		if (!editor)
-			editor = "vi";
-		sprintf(cmd, "xterm -e %s %s", editor, filename);
-		system(cmd);
-	}
-
+//	fd = open(file, O_WRONLY | O_BINARY | O_CREAT | O_APPEND, 0666);
+	fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0666);
+	write(fd, data, strlen(data));
+	close(fd);
 }
 
 int Sys_EnumerateFiles (char *gpath, char *match, int (*func)(char *, int, void *), void *parm)
@@ -389,25 +314,25 @@ int Sys_EnumerateFiles (char *gpath, char *match, int (*func)(char *, int, void 
 
 double Sys_DoubleTime (void)
 {
-    struct timeval tp;
-    struct timezone tzp; 
-    static int      secbase; 
-    
-    gettimeofday(&tp, &tzp);  
+	struct timeval tp;
+	struct timezone tzp; 
+	static int secbase; 
+ 
+	gettimeofday(&tp, &tzp);  
 
-    if (!secbase)
-    {
-        secbase = tp.tv_sec;
-        return tp.tv_usec/1000000.0;
-    }
+	if (!secbase)
+	{
+		secbase = tp.tv_sec;
+		return tp.tv_usec/1000000.0;
+	}
 
-    return (tp.tv_sec - secbase) + tp.tv_usec/1000000.0;
+	return (tp.tv_sec - secbase) + tp.tv_usec/1000000.0;
 }
-
 
 void Sys_UnloadGame (void)
 {
 }
+
 void *Sys_GetGameAPI (void *parms)
 {
 	return NULL;
@@ -424,26 +349,18 @@ void alarm_handler(int x)
 	oktogo=1;
 }
 
-void Sys_LineRefresh(void)
-{
-}
-
-void floating_point_exception_handler(int whatever)
-{
-//	Sys_Warn("floating point exception\n");
-	signal(SIGFPE, floating_point_exception_handler);
-}
-
 char *Sys_ConsoleInput(void)
 {
 #if 0
-    static char text[256];
-    int     len;
+	static char text[256];
+	int len;
 
-	if (cls.state == ca_dedicated) {
+	if (cls.state == ca_dedicated)
+	{
 		len = read (0, text, sizeof(text));
 		if (len < 1)
 			return NULL;
+
 		text[len-1] = 0;    // rip off the /n and terminate
 
 		return text;
@@ -462,18 +379,14 @@ void Sys_LowFPPrecision (void)
 }
 #endif
 
-int		skipframes;
-
 int main (int c, char **v)
 {
-
-	double		time, oldtime, newtime;
+	double time, oldtime, newtime;
 	quakeparms_t parms;
 	int j;
 
 //	static char cwd[1024];
 
-//	signal(SIGFPE, floating_point_exception_handler);
 	signal(SIGFPE, SIG_IGN);
 
 	memset(&parms, 0, sizeof(parms));
@@ -488,6 +401,7 @@ int main (int c, char **v)
 	j = COM_CheckParm("-mem");
 	if (j && j+1 < com_argc)
 		parms.memsize = (int) (Q_atof(com_argv[j+1]) * 1024 * 1024);
+
 	parms.membase = malloc (parms.memsize);
 
 	parms.basedir = basedir;
@@ -503,19 +417,18 @@ int main (int c, char **v)
 
 	Sys_Init();
 
-    Host_Init(&parms);
+	Host_Init(&parms);
 
-    oldtime = Sys_DoubleTime ();
-    while (1)
-    {
+	oldtime = Sys_DoubleTime ();
+	while (1)
+	{
 // find time spent rendering last frame
-        newtime = Sys_DoubleTime ();
-        time = newtime - oldtime;
+		newtime = Sys_DoubleTime ();
+		time = newtime - oldtime;
 
 		Host_Frame(time);
 		oldtime = newtime;
-    }
-
+	}
 }
 
 

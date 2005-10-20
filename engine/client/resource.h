21327eb7e6bed3e67c510f94505aa75f312b5890
Spoike <acceptthis@users.sourceforge.net>
2005-10-20 00:08:23 +0000

    We use morphed's icon now.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@1512 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/client/resource.h
+++ engine/client/resource.h
@@ -9,13 +9,14 @@
 #define IDB_QWBITMAP                    112
 #define IDI_ICON1                       115
 #define IDB_BITMAP1                     116
+#define IDI_ICON3                       122
 #define IDC_PROGRESS                    1000
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        122
+#define _APS_NEXT_RESOURCE_VALUE        123
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101

f784b64de1ff2d9bbeea29c962f6200672decdda
Mark Olsen <isthisinuse@users.sourceforge.net>
2005-11-29 13:20:38 +0000

    Fixed newlines and renamed pixel to pix to avoid name clashes on Altivec systems.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@1621 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/client/m_mp3.c
+++ engine/client/m_mp3.c
@@ -1113,7 +1113,7 @@ qboolean Media_ShowFilm(void)
 			unsigned char *pa=roqfilm->y[0];
 			unsigned char *pb=roqfilm->u[0];
 			unsigned char *pc=roqfilm->v[0];
-			int pixel=0;
+			int pix=0;
 			int num_columns=(roqfilm->width)>>1;
 			int y;
 			int x;
@@ -1141,19 +1141,19 @@ qboolean Media_ShowFilm(void)
 						b = 116130 * u;
 
 						t=r+y1;
-						framedata[pixel] =(unsigned char) LIMIT(t);
+						framedata[pix] =(unsigned char) LIMIT(t);
 						t=g+y1;
-						framedata[pixel+1] =(unsigned char) LIMIT(t);
+						framedata[pix+1] =(unsigned char) LIMIT(t);
 						t=b+y1;
-						framedata[pixel+2] =(unsigned char) LIMIT(t);
+						framedata[pix+2] =(unsigned char) LIMIT(t);
 
 						t=r+y2;
-						framedata[pixel+4] =(unsigned char) LIMIT(t);
+						framedata[pix+4] =(unsigned char) LIMIT(t);
 						t=g+y2;
-						framedata[pixel+5] =(unsigned char) LIMIT(t);
+						framedata[pix+5] =(unsigned char) LIMIT(t);
 						t=b+y2;
-						framedata[pixel+6] =(unsigned char) LIMIT(t);
-						pixel+=8;
+						framedata[pix+6] =(unsigned char) LIMIT(t);
+						pix+=8;
 
 					}
 					if(y & 0x01) { pb += num_columns; pc += num_columns; }

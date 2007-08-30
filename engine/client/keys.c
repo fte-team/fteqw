e5357ebc09a0bef3aa2fde04cabb08fd927f94c6
Molgrum <molgrum@users.sourceforge.net>
2007-08-30 02:31:47 +0000

    Some more strcat-replacements i forgot.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@2616 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/client/keys.c
+++ engine/client/keys.c
@@ -1136,9 +1136,9 @@ void Key_Bind_f (void)
 	cmd[0] = 0;		// start out with a null string
 	for (i=2 ; i< c ; i++)
 	{
-		strcat (cmd, Cmd_Argv(i));
+		Q_strncatz (cmd, Cmd_Argv(i), sizeof(cmd));
 		if (i != (c-1))
-			strcat (cmd, " ");
+			Q_strncatz (cmd, " ", sizeof(cmd));
 	}
 
 	Key_SetBinding (b, modifier, cmd, Cmd_ExecLevel);
@@ -1182,9 +1182,9 @@ void Key_BindLevel_f (void)
 	cmd[0] = 0;		// start out with a null string
 	for (i=3 ; i< c ; i++)
 	{
-		strcat (cmd, Cmd_Argv(i));
+		Q_strncatz (cmd, Cmd_Argv(i), sizeof(cmd));
 		if (i != (c-1))
-			strcat (cmd, " ");
+			Q_strncatz (cmd, " ", sizeof(cmd));
 	}
 
 	Key_SetBinding (b, modifier, cmd, atoi(Cmd_Argv(2)));

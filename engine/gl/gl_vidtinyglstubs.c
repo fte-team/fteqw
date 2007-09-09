f6b0cba2f2165bbef562c43b45d2a427b4982067
Mark Olsen <isthisinuse@users.sourceforge.net>
2007-09-09 14:33:13 +0000

    Added copyright header, extended the comment a bit.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@2646 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/gl/gl_vidtinyglstubs.c
+++ engine/gl/gl_vidtinyglstubs.c
@@ -1,4 +1,23 @@
 /*
+Copyright (C) 2006-2007 Mark Olsen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+/*
 This file is currently used only by bigfoot's MorphOS port.
 
 I should explain this file to any Unix/Windows programmers...
@@ -13,6 +32,9 @@ You can see the state of his opengl library be seeing which functions are actual
 /*
  * PS: It is Kiero's library and everything _IS_ implemented ;)
  *   - bigfoot
+ *
+ * Oh, and just FYI, the offsets are negative, that is, the jump table is below
+ * the library base pointer.
  */
 
 void stub_glAlphaFunc(GLenum func, GLclampf ref)

6a6ae57c845eda5c8151a55cdf374dfe7034e045
Mark Olsen <isthisinuse@users.sourceforge.net>
2007-09-23 09:53:41 +0000

    Added a dummy GL video renderer.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@2681 fc73d0e0-1445-4013-8a0c-d673dee63da5

---
+++ engine/gl/gl_vidnull.c
@@ -0,0 +1,99 @@
+#include "quakedef.h"
+
+qbyte vid_curpal[768];
+
+#ifdef IN_XFLIP
+cvar_t in_xflip = {"in_xflip", "0"};
+#endif
+
+qboolean GLVID_Init(rendererstate_t *info, unsigned char *palette)
+{
+	return false;
+}
+
+void GLVID_DeInit()
+{
+}
+
+void GLVID_ShiftPalette(unsigned char *p)
+{
+}
+
+void GLVID_SetPalette(unsigned char *palette)
+{
+}
+
+void Sys_SendKeyEvents(void)
+{
+}
+
+void GLD_BeginDirectRect(int x, int y, qbyte *pbitmap, int width, int height)
+{
+}
+
+void GLD_EndDirectRect(int x, int y, int width, int height)
+{
+}
+
+void GL_BeginRendering (int *x, int *y, int *width, int *height)
+{
+	*x = *y = 0;
+	*width = 640;
+	*height = 480;
+}
+
+void GL_EndRendering (void)
+{
+}
+
+void GL_DoSwap(void)
+{
+}
+
+void GLVID_LockBuffer (void)
+{
+}
+
+void GLVID_UnlockBuffer (void)
+{
+}
+
+int GLVID_ForceUnlockedAndReturnState (void)
+{
+	return 0;
+}
+
+void GLVID_ForceLockState (int lk)
+{
+}
+
+void GLVID_HandlePause (qboolean pause)
+{
+}
+
+void GLVID_SetCaption(char *text)
+{
+}
+
+/*** Input ***/
+
+void IN_ReInit(void)
+{
+}
+
+void IN_Init(void)
+{
+}
+
+void IN_Shutdown(void)
+{
+}
+
+void IN_Commands(void)
+{
+}
+
+void IN_Move(usercmd_t *cmd, int pnum)
+{
+}
+

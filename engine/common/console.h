12b52d6e11f5d643bfa2242f509ac36b73555d0d
Spoike <acceptthis@users.sourceforge.net>
2010-03-16 19:44:17 +0000

    Fix for gcc.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/branches/wip@3518 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/common/console.h
+++ engine/common/console.h
@@ -144,7 +144,7 @@ void Con_ExecuteLine(console_t *con, char *line);	//takes normal console command
 
 
 void Con_CycleConsole (void);
-qboolean Con_IsActive (console_t *con);
+int Con_IsActive (console_t *con);
 void Con_Destroy (console_t *con);
 void Con_SetActive (console_t *con);
 qboolean Con_NameForNum(int num, char *buffer, int buffersize);

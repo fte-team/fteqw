92451a83058bf329fb5f44c45a50496b3415956b
Lance <moodles2@users.sourceforge.net>
2011-06-03 03:10:39 +0000

    Removed unnecessary defines I added to detect 64bit compilers.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/branches/wip@3808 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/common/plugin.c
+++ engine/common/plugin.c
@@ -265,11 +265,7 @@ int Plug_SystemCallsVM(void *offset, quintptr_t mask, int fn, const int *arg)
 	fn = fn+1;
 
 	if (fn>=0 && fn < numplugbuiltins && plugbuiltins[fn].func!=NULL)
-		#if defined(_M_AMD64) || defined(_M_X64) || defined(__amd64__) || defined(_WIN64)
-			return plugbuiltins[fn].func(offset, mask, (const long long int*)args);
-		#else
-			return plugbuiltins[fn].func(offset, mask, (const long int*)args);
-		#endif
+		return plugbuiltins[fn].func(offset, mask, (qintptr_t*)args);
 #undef args
 	Sys_Error("QVM Plugin tried calling invalid builtin %i", fn);
 	return 0;

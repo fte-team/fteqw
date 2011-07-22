c088ddf18e7e537f6db43fdbecf032703be776ce
TimeServ <timeserv@users.sourceforge.net>
2011-07-22 20:26:22 +0000

    remove extra check from channel picking
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@3887 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/client/snd_dma.c
+++ engine/client/snd_dma.c
@@ -1255,7 +1255,7 @@ channel_t *SND_PickChannel(soundcardinfo_t *sc, int entnum, int entchannel)
     {
 		if (entchannel != 0		// channel 0 never overrides
 		&& sc->channel[ch_idx].entnum == entnum
-		&& (sc->channel[ch_idx].entchannel == entchannel || entchannel == -1) && sc->channel[ch_idx].end > snd_speed/10)
+		&& (sc->channel[ch_idx].entchannel == entchannel || entchannel == -1))
 		{	// always override sound from same entity
 			first_to_die = ch_idx;
 			break;

b39b0fae51048b14fcbd9238edbedb6f287f4c67
Mark Olsen <isthisinuse@users.sourceforge.net>
2012-04-23 18:43:12 +0000

    Not that it is accepting server announcements currently anyway, but changed asgaard.morphos-team.net to qwmaster.fodquake.net.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@4027 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/client/net_master.c
+++ engine/client/net_master.c
@@ -1503,7 +1503,7 @@ void MasterInfo_Refresh(void)
 			Master_AddMaster("master.quakeservers.net:27000",	MT_MASTERQW, "QuakeServers.net");
 			Master_AddMaster("qwmaster.fodquake.net:27000",		MT_MASTERQW, "Fodquake.net");
 			Master_AddMaster("masterserver.exhale.de:27000",	MT_MASTERQW, "team exhale");
-			Master_AddMaster("asgaard.morphos-team.net:27000",		MT_MASTERQW, "Asgaard master server.");
+			Master_AddMaster("qwmaster.fodquake.net:27000",		MT_MASTERQW, "Fodquake master server.");
 			Master_AddMaster("qwmaster.ocrana.de:27000",		MT_MASTERQW, "Ocrana2 master server.");
 			Master_AddMaster("255.255.255.255:27500",		MT_BCASTQW, "Nearby QuakeWorld UDP servers.");
 		}

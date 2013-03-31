b5240d63352aa3b80257f5735166759b0dca7852
Spoike <acceptthis@users.sourceforge.net>
2013-03-31 04:36:21 +0000

    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@4275 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/server/sv_sys_unix.c
+++ engine/server/sv_sys_unix.c
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <signal.h>
 #include <sys/types.h>
 #include <dlfcn.h>
-#include "qwsvdef.h"
+#include "quakedef.h"
 
 
 #undef malloc

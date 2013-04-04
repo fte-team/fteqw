586eee6a60f6ba021eb4cbc85a777a958c6915dc
Spoike <acceptthis@users.sourceforge.net>
2013-04-04 18:04:48 +0000

    try to be more conservative, just to show something works.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@4287 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- engine/web/prejs.js
+++ engine/web/prejs.js
@@ -6,11 +6,11 @@ Module['preRun'] = function()
 
 	//FS.createPath('/', 'tmp', true, true);
 
-	FS.createPreloadedFile('/id1/', 'pak0.pak', '/pak0.pak', true, false);
+	//FS.createPreloadedFile('/id1/', 'pak0.pak', '/pak0.pak', true, false);
 	//FS.createPreloadedFile('/id1/', 'pak1.pak', '/pak1.pak', true, false);
 	//FS.createPreloadedFile('/id1/', 'pak2.pak', '/pak2.pak', true, false);
 	//FS.createPreloadedFile('/id1/', 'pak3.pak', '/pak3.pak', true, false);
 };
 
-Module['arguments'] = ['-nohome', '+connect', 'tcp://127.0.0.1:80'];//, '-manifest', document.location + '.fmf'];
+Module['arguments'] = ['-nohome'];//, '+connect', 'tcp://127.0.0.1:80'];//, '-manifest', document.location + '.fmf'];
 

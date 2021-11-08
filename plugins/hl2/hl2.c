462f3e4a2042ec6fa79622218f3209c1a3ee1287
Spoike <acceptthis@users.sourceforge.net>
2021-11-08 08:47:05 +0000

    Basic vmt support. Requisite GLSL files are still separate.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@6114 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- plugins/hl2/hl2.c
+++ plugins/hl2/hl2.c
@@ -2,12 +2,14 @@
 qboolean VPK_Init(void);
 qboolean VTF_Init(void);
 qboolean MDL_Init(void);
+qboolean VMT_Init(void);
 
 qboolean Plug_Init(void)
 {
 	if (!VPK_Init())
 		return false;
 	VTF_Init();
+	VMT_Init();
 	MDL_Init();
 	return true;
 }

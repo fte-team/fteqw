0f8ea718414d2b62f2898186ae6c467d3109b082
Spoike <acceptthis@users.sourceforge.net>
2021-11-08 08:46:57 +0000

    Add hl2-mdl support to the hl2 plugin.
    git-svn-id: https://svn.code.sf.net/p/fteqw/code/trunk@6113 fc73d0e0-1445-4013-8a0c-d673dee63da5

--- plugins/hl2/hl2.c
+++ plugins/hl2/hl2.c
@@ -1,13 +1,14 @@
 #include "../plugin.h"
 qboolean VPK_Init(void);
 qboolean VTF_Init(void);
+qboolean MDL_Init(void);
 
 qboolean Plug_Init(void)
 {
 	if (!VPK_Init())
 		return false;
-	if (!VTF_Init())
-		return false;
+	VTF_Init();
+	MDL_Init();
 	return true;
 }
 

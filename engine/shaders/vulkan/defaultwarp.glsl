84438b5f9b738cc948a33e282b5327475acdcf60
Shpoike <Shpoike@users.noreply.github.com>
2024-10-01 03:37:12 +0100

    vk: Add lit water support to the spir-v.

--- engine/shaders/vulkan/defaultwarp.glsl
+++ engine/shaders/vulkan/defaultwarp.glsl
@@ -2,7 +2,8 @@
 !!cvarf r_wateralpha=1
 !!cvarb r_fog_exp2=true
 !!argf alpha=0
-!!samps diffuse
+!!argb lit=false
+!!samps diffuse lightmap
 
 #include "sys/defs.h"
 
@@ -11,6 +12,7 @@
 
 #include "sys/fog.h"
 layout(location=0) varying vec2 tc;
+layout(location=1) varying vec2 lmtc;
 
 #ifdef VERTEX_SHADER
 void main ()
@@ -19,16 +21,19 @@ void main ()
 	#ifdef FLOW
 	tc.s += e_time * -0.5;
 	#endif
+	lmtc = v_lmcoord.st;
 	gl_Position = ftetransform();
 }
 #endif
 #ifdef FRAGMENT_SHADER
 void main ()
 {
-	vec2 ntc;
-	ntc.s = tc.s + sin(tc.t+e_time)*0.125;
-	ntc.t = tc.t + sin(tc.s+e_time)*0.125;
+	vec2 ntc = tc + sin(tc.ts+e_time)*0.125;
 	vec3 ts = vec3(texture2D(s_diffuse, ntc));
+
+	if (arg_lit)
+		ts *= (texture2D(s_lightmap, lmtc) * e_lmscale).rgb;
+
 	if (arg_alpha != 0.0)
 		gl_FragColor = fog4(vec4(ts, arg_alpha));
 	else

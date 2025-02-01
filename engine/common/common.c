c03c97a578fd6c175deb66a620bb0045633af09f
erysdren <contact@erysdren.me>
2025-02-01 03:14:07 -0600

    fix FTE_WEB compilation for emcc 3.1.72-git

--- engine/common/common.c
+++ engine/common/common.c
@@ -25,6 +25,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <ctype.h>
 #include <errno.h>
 
+#ifdef FTE_TARGET_WEB
+#include <emscripten/version.h>
+#endif
+
 qboolean sys_nounload;
 double		host_frametime;
 double		realtime;				// without any filtering or bounding
